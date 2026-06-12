eaa30f6d2b03dd1769ef1b6034d4eedfac80d330
tyagidevansh <devanshtyagi0607@gmail.com>
2026-06-13 01:01:13 +0530

    new enemy + level 1 map

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -5,16 +5,20 @@ var direction = 1
 
 @onready var ray_cast_right: RayCast2D = $RayCastRight
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
+@onready var ground_check_right: RayCast2D = $GroundCheckRight
+@onready var ground_check_left: RayCast2D = $GroundCheckLeft
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if ray_cast_right.is_colliding():
-		direction = -1
-		animated_sprite.flip_h = true
+	if direction == 1:
+		if ray_cast_right.is_colliding() or !ground_check_right.is_colliding():
+			direction = -1
+			animated_sprite.flip_h = true
 	
-	if ray_cast_left.is_colliding():
-		direction = 1
-		animated_sprite.flip_h = false
+	else:
+		if ray_cast_left.is_colliding() or !ground_check_left.is_colliding():
+			direction = 1
+			animated_sprite.flip_h = false
 		
 	position.x += direction * SPEED * delta
--- scripts/player.gd
+++ scripts/player.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -15,9 +15,22 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Input directions: -1, 0 or 1
 	var direction := Input.get_axis("move_left", "move_right")
+	
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+	
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		animated_sprite.play("jump")
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
